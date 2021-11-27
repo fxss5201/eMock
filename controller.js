0a49effe44a86d4f0241383e91445a024e6207ae
fanyushun <fanyushun@zj.tech>
2021-11-27 22:04:28 +0800

    feat: 支持文件夹

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 // https://github.com/michaelliao/learn-javascript/blob/master/samples/node/web/koa/url2-koa/controller.js
 
 const fs = require('fs')
+const path = require('path')
 const chalk = require('chalk')
 
 /**
@@ -8,7 +9,7 @@ const chalk = require('chalk')
  * @param {Object} router require('koa-router')()
  * @param {Object} mapping require(__dirname + '/' + dir + '/' + f)
  */
-function addMapping (router, mapping) {
+function addMapping(router, mapping) {
   for (const url in mapping) {
     if (url.toLowerCase().startsWith('get ')) {
       const path = url.substring(4)
@@ -37,14 +38,29 @@ function addMapping (router, mapping) {
  * @param {Object} router require('koa-router')()
  * @param {String} dir path
  */
-function addControllers (router, dir) {
-  fs.readdirSync(__dirname + '/' + dir).filter((f) => {
-    return f.endsWith('.js')
-  }).forEach((f) => {
-    console.log(chalk.blue(`process controller: ${f}...`))
-    const mapping = require(__dirname + '/' + dir + '/' + f)
-    addMapping(router, mapping)
-  })
+function addControllers(router, dir) {
+  const fullpath = path.join(__dirname + '/' + dir)
+  listFile(router, fullpath)
+}
+
+/**
+ * 递归循环深层目录便利
+ * @param {Object} router require('koa-router')()
+ * @param {String} dirPath path
+ */
+function listFile(router, dirPath) {
+  const arr = fs.readdirSync(dirPath)
+  arr.forEach(function (item) {
+    const fullpath = path.join(dirPath, item)
+    const stats = fs.statSync(fullpath)
+    if (stats.isDirectory()) {
+      listFile(router, fullpath);
+    } else {
+      console.log(chalk.blue(`process controller: ${fullpath}...`))
+      const mapping = require(fullpath)
+      addMapping(router, mapping)
+    }
+  });
 }
 
 module.exports = function (dir) {
